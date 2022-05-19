667767872a9397c14c755a88ecc3ac1a83caa48a
Daniel Kelly <me@danielkelly.io>
2022-05-19 08:58:20 -0500

    fix build issue with TS

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,10 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Pages from "vite-plugin-pages";
-import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls },
-    }),
-    Pages(),
-    quasar({
-      autoImportComponentCase: "pascal",
-      sassVariables: "src/assets/quasar-variables.sass",
-    }),
-  ],
+  plugins: [vue({}), Pages()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
